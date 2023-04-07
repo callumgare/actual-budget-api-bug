935c833f3389ae1cec9721bf982e6de22db99fe7
Callum Gare <callum@gare.au>
2023-04-08 07:55:50 +1000

    Modify to demonstrate out-of-sync-migrations bug

--- index.js
+++ index.js
@@ -20,7 +20,12 @@ async function sync() {
   console.log("Downloading ActualBudget cache locally")
   await actualBudget.downloadBudget(syncID);
 
-  await actualBudget.importTransactions(accountId, transactions)
+  const results = await actualBudget.importTransactions(accountId, transactions)
+  console.log(results)
+
+  // Call downloadBudget again to force a sync to occur. Otherwise if we shutdown the connection immediately after an 
+  // import the transactions without calling downloadBudget again the transactions will not have finished syncing yet.
+  await actualBudget.downloadBudget(syncID);
 
   await actualBudget.shutdown()
 }
@@ -31,22 +36,25 @@ const transactions = [
   {
     account: accountId,
     date: '2023-03-26',
-    payee: 'VISA PURCHASE MARLEYSPOON.COM.AU',
+    amount: 4230,
+    payee_name: 'VISA PURCHASE MARLEYSPOON.COM.AU',
     imported_payee: 'VISA PURCHASE MARLEYSPOON.COM.AU',
-    transfer_id: 'ztC8ZxeH4tkqob5q84MaWTWkJ1nGlmTH0zsxNzUraiE='
+    imported_id: 'ztC8ZxeH4tkqob5q84MaWTWkJ1nGlmTH0zsxNzUraiE='
   },
   {
     account: accountId,
     date: '2023-03-26',
-    payee: 'VISA PURCHASE Live Payments',
+    amount: -200,
+    payee_name: 'VISA PURCHASE Live Payments',
     imported_payee: 'VISA PURCHASE Live Payments',
-    transfer_id: 'd0xHQsat8HT5vzd5SbZIt4gCJVGwG0Mfevza+1da8aM='
+    imported_id: 'd0xHQsat8HT5vzd5SbZIt4gCJVGwG0Mfevza+1da8aM='
   },
   {
     account: accountId,
     date: '2023-03-26',
-    payee: 'VISA PURCHASE BRUNSWICK SUPA IGA',
+    amount: 600,
+    payee_name: 'VISA PURCHASE BRUNSWICK SUPA IGA',
     imported_payee: 'VISA PURCHASE BRUNSWICK SUPA IGA',
-    transfer_id: 'FvSCGi9CZY7JviDXVquLzm4eJCd8Jc6DC/v5m7XXuXU='
+    imported_id: 'FvSCGi9CZY7JviDXVquLzm4eJCd8Jc6DC/v5m7XXuXU='
   }
 ]
